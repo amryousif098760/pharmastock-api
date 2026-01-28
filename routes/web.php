60d3554b155fa110b3ac33bcad298b390bdb27aa
amryousif098760 <amryousif09876@gmail.com>
2026-01-28 02:34:49 +0200

    Add email verification route to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/api/auth/verify-email', [AuthController::class, 'verifyEmailLink']);
 
 Route::get('/', function () {
     return view('welcome');
