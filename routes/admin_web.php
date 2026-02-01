3d757f4e7fd77184bac25b0d1ff863f53772d025
amryousif098760 <amryousif09876@gmail.com>
2026-02-02 00:00:43 +0200

    Update admin_web.php

--- routes/admin_web.php
+++ routes/admin_web.php
@@ -18,11 +18,7 @@ Route::post('/logout', [AdminAuthController::class, 'logout'])->name('admin.logo
 
 Route::middleware(['auth', 'is_admin'])->group(function () {
 
-    Route::get('/dashboard', function () {
-    return response('DASHBOARD_ROUTE_OK', 200);
-})->name('admin.dashboard')->middleware(['auth','is_admin']);
-
-
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
     Route::get('/banners', [BannersController::class, 'index'])->name('admin.banners.index');
     Route::get('/banners/create', [BannersController::class, 'create'])->name('admin.banners.create');
     Route::post('/banners', [BannersController::class, 'store'])->name('admin.banners.store');
