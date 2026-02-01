7c36b622d12d44519239a207a9d3fb18c0ff42da
amryousif098760 <amryousif09876@gmail.com>
2026-02-02 00:30:25 +0200

    Update Medicine.php

--- app/Models/Medicine.php
+++ app/Models/Medicine.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Medicine extends Model
 {
-    protected $fillable = ['warehouse_id', 'category_id', 'name', 'price', 'qty', 'image_url'];
+    protected $fillable = [
+        'warehouse_id',
+        'category_id',
+        'name',
+        'price',
+        'qty',
+        'image_url',
+        'is_featured',
+    ];
+
+    public function warehouse(): BelongsTo
+    {
+        return $this->belongsTo(Warehouse::class, 'warehouse_id');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
