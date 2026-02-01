3703d68363361acb79f3b81563172857e7efadf8
amryousif098760 <amryousif09876@gmail.com>
2026-02-01 22:36:25 +0200

    Add admin route group to application routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,11 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            \Illuminate\Support\Facades\Route::middleware('web')
+                ->prefix('admin')
+                ->group(__DIR__.'/../routes/admin_web.php');
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
@@ -17,3 +22,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
