df98ea7d67c884aae66a8fe72c911e147989253c
amryousif098760 <amryousif09876@gmail.com>
2026-02-02 15:54:48 +0200

    Update Pharmacy.php

--- app/Models/Pharmacy.php
+++ app/Models/Pharmacy.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Pharmacy extends Model
 {
     protected $fillable = ['user_id', 'name', 'lat', 'lng', 'address_text'];
+
+    public function user()
+    {
+        return $this->belongsTo(\App\Models\User::class, 'user_id');
+    }
 }
